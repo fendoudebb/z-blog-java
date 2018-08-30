71c99e74dd42657c6dbb58afa37b87cc415543f2
zhangbj94 <zhangbj94@chinaunicom.cn>
2018-08-31 07:13:10 +0800

    错误页

---
+++ src/main/java/com/msj/blog/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.msj.blog.controller;
+
+import com.msj.blog.util.IpUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class IndexController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        log.info(IpUtil.getIpAddress() + ": get error path");
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
