e7d0dcff8d3efeecf9bdd25e71b6a1a7335f43d4
zbj <15821722108@163.com>
2018-09-20 22:16:00 +0800

    调整代码

--- src/main/java/com/msj/blog/entity/dto/page/PageDto.java
+++ src/main/java/com/msj/blog/entity/dto/page/PageDto.java
@@ -3,7 +3,6 @@ package com.msj.blog.entity.dto.page;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 /**
